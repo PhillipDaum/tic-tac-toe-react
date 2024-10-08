671f090ce1f44b71b7708f9fa8ae2359f67be216
PhillipDaum <philgdaum@gmail.com>
2024-10-07 18:41:03 -0600

    add first components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Gameboard from './components/Gameboard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <form id="form">
+        <label for="X">X</label>
+            <input type="radio" name="humanSymbol" id="X" value="X"></input>
+        <label for="O">O</label>
+            <input type="radio" name="humanSymbol" id="O" value="O"></input>
+        <button type="submit" id="submit" control-id="ControlID-3">New game (vs cpu)</button>
+    </form>
+    <Gameboard /> 
     </>
   )
 }
+
+
 
 export default App
---
+++ src/components/Gameboard.jsx
@@ -0,0 +1,21 @@
+function Gameboard() {
+  return (
+    <>
+        <section className="grid">
+            <div className="cell empty" id="0"></div>
+            <div className="cell empty" id="1"></div>
+            <div className="cell empty" id="2"></div>
+            <div className="cell empty" id="3"></div>
+            <div className="cell empty" id="4"></div>
+            <div className="cell empty" id="5"></div>
+            <div className="cell empty" id="6"></div>
+            <div className="cell empty" id="7"></div>
+            <div className="cell empty" id="8"></div>
+        </section>
+    </>
+  )
+}
+
+
+
+export default Gameboard
